50095d3fcf13d1b54d7396af9e7a3eb6ee8f8349
vc6-1998 <70468619+vc6-1998@users.noreply.github.com>
2020-09-13 16:22:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/t
 sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
 sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
 pkg update
+y
 pkg install vim curl wget git tree -y clang python
 sh -c "$(curl -fsSL https://html.sqlsec.com/termux-install.sh)"
