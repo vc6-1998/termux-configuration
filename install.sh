169a390794457234975b11e68b50ce7ebf4242a3
vc6-1998 <70468619+vc6-1998@users.noreply.github.com>
2020-09-13 15:42:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-pkg install clang
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+pkg update
+pkg install vim curl wget git tree -y
+sh -c "$(curl -fsSL https://html.sqlsec.com/termux-install.sh)"  
